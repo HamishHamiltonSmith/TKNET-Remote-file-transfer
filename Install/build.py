c2b8ca381787dd716d5c13d981684eca32ef2dda
Hamish Hamilton-Smith <83606175+HamishHamiltonSmith@users.noreply.github.com>
2021-12-08 00:26:46 +0000

    Add files via upload

--- Install/build.py
+++ Install/build.py
@@ -4,6 +4,12 @@ print('[+] Preparing file system...')
 os.system('mkdir /usr/share/Tknet')
 
 print('[+] Copying files')
-os.system('cp -r ~/Downloads/Tknet/Src/Server /usr/share/Tknet')
-os.system('cp -r ~/Downloads/Tknet/Src/Client /usr/share/Tknet')
-os.system('cp ~/Downloads/Tknet/Src/Command/tknet /bin')
+d = str(input("Where is the tknet folder (path eg:/home/Downloads) "))
+
+if d[len(d)-1] == '/':
+    d = d[:len(d)-1]
+
+
+os.system(f'cp -r {d}/Tknet/Src/Server /usr/share/Tknet')
+os.system(f'cp -r {d}/Tknet/Src/Client /usr/share/Tknet')
+os.system(f'cp {d}/Tknet/Src/Command/tknet /bin')
